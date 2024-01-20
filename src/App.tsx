2aee4100b755fab9d1a87901eab69d723cd25424
Ryouichi Matsuda <ryouichi.matsuda@nifty.ne.jp>
2024-01-20 18:00:58 +0900

    modify layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,30 @@
 import { ActorList } from "./components/list/actor-list";
-import { DiagramZoom } from "./components/diagram-zoom/diagram-zoom";
 import { Menu } from "./components/menu/menu";
 import { ProcessList } from "./components/list/process-list";
 import { ToolIconButtons } from "./components/tool-icon-buttons/tool-icon-buttons";
 import "./app.css";
-import { useModel } from "./context";
 import { ProcessDialog } from "./components/dialog/process-dialog";
 import { PackageDialog } from "./components/dialog/package-dialog";
+import { Main } from "./components/main/main";
 
 function App() {
-  const {
-    process: { selectedProcess },
-  } = useModel();
-
   return (
     <>
       <div class="app">
-        <Menu />
-        <div class="main">
-          <div class="split-left">
-            <ProcessList />
-            <div class="v-divide"></div>
-            <ActorList />
-          </div>
-          <div class="h-divide"></div>
-          <div class="split-right">
-            <ToolIconButtons />
-            <div class="editor">
-              <h5>{selectedProcess().title}</h5>
-              <canvas class="diagram" />
-              <DiagramZoom />
-            </div>
-          </div>
+        <div class="app__menu">
+          <Menu />
+        </div>
+        <div class="app__process-list">
+          <ProcessList />
+        </div>
+        <div class="app__actor-list">
+          <ActorList />
+        </div>
+        <div class="app__tool-button">
+          <ToolIconButtons />
+        </div>
+        <div class="app__main">
+          <Main />
         </div>
       </div>
 
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,30 +1,17 @@
-import { ActorList } from "../list/actor-list";
 import { DiagramZoom } from "../diagram-zoom/diagram-zoom";
-import { Menu } from "../menu/menu";
-import { ProcessList } from "../list/process-list";
-import { ToolIconButtons } from "../tool-icon-buttons/tool-icon-buttons";
-import "./app.css";
+import "./main.css";
+import { useModel } from "../../context";
 
-export function App() {
+export function Main() {
+  const {
+    process: { selectedProcess },
+  } = useModel();
+
   return (
     <div class="main">
-      <Menu />
-      <div class="container">
-        <div class="split-left">
-          <ProcessList />
-          <div class="v-divide"></div>
-          <ActorList />
-        </div>
-        <div class="h-divide"></div>
-        <div class="split-right">
-          <ToolIconButtons />
-          <div class="editor">
-            <h5>プロセス1</h5>
-            <canvas class="diagram" />
-            <DiagramZoom />
-          </div>
-        </div>
-      </div>
+      <h5>{selectedProcess().title}</h5>
+      <canvas class="main__diagram" />
+      <DiagramZoom />
     </div>
   );
 }
